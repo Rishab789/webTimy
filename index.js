332cc75a948d9b2c37ea7d5405a3399811a96329
Rishab789 <beraprakash456@gmail.com>
2023-11-21 11:41:01 +0530

    Commit First

---
+++ index.js
@@ -0,0 +1,11 @@
+// function hamburger() {
+
+// }
+
+
+let hamburger = document.querySelector(".fa-bars");
+hamburger.addEventListener('click', () => {
+    let nav = document.querySelector('nav');
+
+    nav.classList.toggle('active')
+})
